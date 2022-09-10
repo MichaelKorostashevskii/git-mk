9ac70e3307adae216919837529086526ad45f693
MichaelKorostashevskii <michaelkorostashevskii@gmail.com>
2022-09-10 09:58:05 +0300

    feat2

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,4 +2,7 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("master 1");
     }
+    public static void work(){
+        System.out.println("feat2");
+    }
 }
