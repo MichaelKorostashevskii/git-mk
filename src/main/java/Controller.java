f40bb1f16ee51afc0b6d33e261cab051ffca43b5
MichaelKorostashevskii <michaelkorostashevskii@gmail.com>
2022-09-10 10:28:16 +0300

    feat 3

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,6 +2,8 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("master 1");
         System.out.println("feat1");
+
+        //TODO feat 4
     }
     public static void work(){
         System.out.println("feat2");
