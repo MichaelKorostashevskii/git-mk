5463d2c5cf2cf8a5980ee1d11fbe585335d998d2
MichaelKorostashevskii <michaelkorostashevskii@gmail.com>
2022-09-18 19:50:53 +0300

    my class

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,6 +2,7 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("master 1");
         System.out.println("feat1");
+        work();
     }
     public static void work(){
         System.out.println("feat2");
